5a35c7a543596c9309566674582a48f77bf2c0ea
Jorden_Reuter <149687553+JordenReuter@users.noreply.github.com>
2026-04-30 08:47:35 +0200

    build(deps): bump org.tkit.onecx:onecx-quarkus3-parent from 2.5.0 to 3.1.0

--- src/test/java/org/tkit/onecx/ai/bff/rs/controllers/ProviderRestControllerTest.java
+++ src/test/java/org/tkit/onecx/ai/bff/rs/controllers/ProviderRestControllerTest.java
@@ -43,7 +43,7 @@ class ProviderRestControllerTest extends AbstractTest {
     void resetMockserver() {
         try {
             mockServerClient.clear(MOCK_ID);
-        } catch (Exception ex) {
+        } catch (Exception _) {
             // mockId not existing
         }
     }
