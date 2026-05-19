bd7d9be76d33d4feaabdbe29b49a74f072477910
Jorden_Reuter <149687553+JordenReuter@users.noreply.github.com>
2026-05-19 14:04:36 +0200

    feat: added ITs

---
+++ src/test/java/org/tkit/onecx/ai/bff/rs/controllers/ProviderRestControllerTestIT.java
@@ -0,0 +1,7 @@
+package org.tkit.onecx.ai.bff.rs.controllers;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class ProviderRestControllerTestIT extends ProviderRestControllerTest {
+}
